d23db9341aac7ab7e41191307e9238712dbfeeb8
Cassandre <cassandre.michelet@gmail.com>
2021-06-09 23:15:25 +0200

    Main design css + changes file's names

--- src/Controller/IceicebabyController.php
+++ src/Controller/IceicebabyController.php
@@ -5,20 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
 
 class IceicebabyController extends AbstractController
 {
     /**
-    * @Route("/iceicebaby", name="iceicebaby")
+    * @Route("/", name="home")
     */
-    public function index(): Response
+    public function home() {
+        return $this->render('iceicebaby/home.html.twig', [
+            'title1' => "ICE CREAM",
+            'title2' => "ICE DESSERT",
+            'title3' => "NOS ICE & VOUS"
+        ]);
+    }
+
+    /**
+    * @Route("/icecream", name="icecream")
+    */
+    public function icecream(): Response
     {
-        return $this->render('iceicebaby/index.html.twig', [
+        $repo = $this->getDoctrine()->getRepository(Product::class);
+
+        $products = $repo->findByProductType("Type de produit glace/sorbet/ice stick/bûche/ice entremet");
+
+        return $this->render('iceicebaby/icecream.html.twig', [
             'controller_name' => 'IceicebabyController',
             'title1' => "GLACES",
             'title2' => "SORBETS",
             'title3' => "ICE STICKS",
-            'title4' => "CORNETS"
+            'title4' => "CÔNES",
+            'products' => $products
         ]);
     }
 

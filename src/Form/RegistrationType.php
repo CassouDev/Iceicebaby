ed48144abb69240a5fb61bc5e0af9685c6541475
Cassandre <cassandre.michelet@gmail.com>
2021-06-24 21:39:18 +0200

    SECURITY / Registration form

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('userName', TextType::class)
+            ->add('userLastname', TextType::class)
+            ->add('userMail', EmailType::class)
+            ->add('userPhoneNumber', TelType::class)
+            ->add('password', PasswordType::class)
+            ->add('confirmPassword', PasswordType::class)
+            ->add('userAdress', TextType::class)
+            ->add('userComplement', TextType::class)
+            ->add('userCity', TextType::class)
+            ->add('userPostcode')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
